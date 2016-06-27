adab81e17e397d019e1d993f8383a3bb110b0415
Clang <clangcn@gmail.com>
2016-06-27 11:30:45 +0800

    Update install-kcp-server.sh

--- install-kcp-server.sh
+++ install-kcp-server.sh
@@ -286,7 +286,7 @@ function pre_install_clang(){
 # Config file
 cat > ${str_program_dir}/config.json<<-EOF
 {
-    "server":"0.0.0.0",
+    "server":"${defIP}",
     "redir_port":0,
     "tuncrypt":0,
     "sndwnd":128,
